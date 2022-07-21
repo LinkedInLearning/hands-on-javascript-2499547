a3de2a7933a5a9a3a414eb7fd268cfb7d4e0d0c9
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2022-07-21 13:01:11 -0700

    modify 01_03

--- 01_03b/script.js
+++ 01_03b/script.js
@@ -1,3 +1,10 @@
+/**
+ * Nesting components:
+ * - Create a new cardlist component.
+ * - Iterate through all available images in `data.js`.
+ * - Output a card for each available image.
+ */
+
 import data from "./data.js";
 import Card from "./components/Card.js";
 
--- 01_03e/script.js
+++ 01_03e/script.js
@@ -1,3 +1,10 @@
+/**
+ * Nesting components:
+ * - Create a new cardlist component.
+ * - Iterate through all available images in `data.js`.
+ * - Output a card for each available image.
+ */
+
 import data from "./data.js";
 import Cardlist from "./components/Cardlist.js";
 
