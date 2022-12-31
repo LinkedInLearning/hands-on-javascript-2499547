cdc21cdfbd271eeecf626aa348bb3967e4c75041
Chad F <31939029+c-Fancy@users.noreply.github.com>
2022-12-31 02:27:22 +0000

    Added a date

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -12,6 +12,8 @@ const mainContent = document.querySelector(".main-content");
 const Card = (data) => {
   const imgData = data[0];
 
+  const date = new Date(imgData.created_at);
+
   const markup = `
     <figure class="image">
       <img
@@ -33,6 +35,16 @@ const Card = (data) => {
           <p>
             Photo by
             <span class="image__photog">${imgData.user.name}</span>.
+          </p>
+          <p>
+          Uploaded on
+          <time class="image_date" datetime="${
+            imgData.created_at
+          }">${date.toLocaleString("default", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  })}</time>
           </p>
           <p>
             <a href="${imgData.links.self}" class="image__link">
