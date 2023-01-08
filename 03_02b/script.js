e9abf16d636f94fc5d9c8df53a82d3aebee73149
Josh McIntyre <josh@magnesioweb.com>
2023-01-08 15:16:26 +0000

    03_02: add fetch data from openweather API

--- 03_02b/script.js
+++ 03_02b/script.js
@@ -12,3 +12,20 @@ import settings from "../settings.js";
 const tempField = document.querySelector(".getTemp");
 const windSpeed = document.querySelector(".getWSpeed");
 const windDir = document.querySelector(".getWDir");
+
+const displayData = () => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${settings.location}&appid=${settings.appid}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      tempField.innerHTML = data.main.temp;
+      windSpeed.innerHTML = data.wind.speed;
+      windDir.innerHTML = data.wind.deg;
+    });
+};
+
+displayData();
--- settings.js
+++ settings.js
@@ -1,6 +1,8 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  // Morten's API key at time of tutuorial - e8274867b47796dc78e4c0c08e9f9cb8
+
+  appid: "bdeae396f55f2a0ff6afb0524f051d5c",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "nashville,tn,us",
 };
