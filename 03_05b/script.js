0f1d6bdf69f5f91cab803e70e0f8ad99af3248c1
Josh McIntyre <josh@magnesioweb.com>
2023-01-08 17:50:19 +0000

    03_05: add geo lookup API

--- 03_05b/script.js
+++ 03_05b/script.js
@@ -28,24 +28,34 @@ const unitChanger = () => {
   const unitsButton = document.querySelector("#units");
   unitsButton.addEventListener("click", () => {
     units === "metric" ? (units = "imperial") : (units = "metric");
-    displayData(units);
+    displayData(location, units);
   });
 };
 
 async function displayData(location, units) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${settings.appid}`
+  const currentLoc = await fetch(
+    `https://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${settings.appid}`
   )
-    .then(function (response) {
-      return response.json();
-    })
+    .then((response) => response.json())
     .then(function (data) {
-      console.log(data);
-      mainContent.innerHTML = weatherCard(data, units);
-    })
-    .then(function () {
-      unitChanger();
+      return data;
     });
+
+  if (currentLoc) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${currentLoc[0].lat}&lon=${currentLoc[0].lon}&APPID=${settings.appid}`
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        mainContent.innerHTML = weatherCard(data, units);
+      })
+      .then(function () {
+        unitChanger();
+      });
+  }
 }
 
 displayData(location, units);
