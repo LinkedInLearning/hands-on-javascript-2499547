afbd14569fd9368f486c39910d277324999368e5
Chad F <31939029+c-Fancy@users.noreply.github.com>
2023-01-08 14:18:59 +0000

    Added a const for img sizing

--- 01_01b/script.js
+++ 01_01b/script.js
@@ -9,18 +9,19 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
-const Card = (data) => {
-  const imgData = data[0];
+const buildImage = (imgData) => {
+  let srcset = `${imgData.urls.full} ${imgData.width}w`;
+  if (imgData.urls.regular) {
+    srcset = srcset + `,${imgData.urls.regular} 1080w`;
+  }
+  if (imgData.urls.small) {
+    srcset = srcset + `${imgData.urls.small} 400w`;
+  }
 
-  const date = new Date(imgData.created_at);
-
-  const markup = `
-    <figure class="image">
-      <img
+  const img = `
+  <img
         srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
+          ${srcset}
         "
         sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
         src="${imgData.urls.regular}"
@@ -28,7 +29,26 @@ const Card = (data) => {
         height="${imgData.height}"
         alt="${imgData.description}"
         loading="lazy"
-      />
+      />`;
+  return img;
+};
+
+const getDate = (imgData) => {
+  const date = new Date(imgData.created_at);
+  const niceDate = date.toLocaleString("default", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+  return niceDate;
+};
+
+const Card = (data) => {
+  const imgData = data[0];
+
+  const markup = `
+    <figure class="image">
+      ${buildImage(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
@@ -38,13 +58,9 @@ const Card = (data) => {
           </p>
           <p>
           Uploaded on
-          <time class="image_date" datetime="${
-            imgData.created_at
-          }">${date.toLocaleString("default", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  })}</time>
+          <time class="image_date" datetime="${imgData.created_at}">${getDate(
+    imgData
+  )}</time>
           </p>
           <p>
             <a href="${imgData.links.self}" class="image__link">
