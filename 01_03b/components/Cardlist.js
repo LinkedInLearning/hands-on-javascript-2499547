3e3e9b873bb1bc87adfbd28823193f64e401e7ef
Chad F <31939029+c-Fancy@users.noreply.github.com>
2023-01-08 22:00:14 +0000

    Created a component reusable through nesting

---
+++ 01_03b/components/Cardlist.js
@@ -0,0 +1,22 @@
+import Card from "./Card.js";
+
+const cardListItem = (imgData) => {
+  return `
+  <li class="cardList__item">
+    ${Card(imgData)}
+  </li>
+  `;
+};
+
+const Cardlist = (data) => {
+  return `
+    <link href="components/cardlist.css" rel="stylesheet" />
+    <section class="cardlist">
+      <ul class="cardlist__list">
+        ${data.map((imgData) => cardListItem(imgData)).join("")}
+      </ul>
+    </section>
+    `;
+};
+
+export default Cardlist;
