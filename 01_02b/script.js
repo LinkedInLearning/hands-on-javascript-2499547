9157001297194d2cb317ec8c6f936d84aa8d6dc9
fromOkayToGreat <88689386+fromOkayToGreat@users.noreply.github.com>
2023-03-10 22:13:45 +0000

    done: 01_02b/script.js

--- 01_02b/script.js
+++ 01_02b/script.js
@@ -8,25 +8,54 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
-const Card = (data) => {
-  const imgData = data[0];
-  const date = new Date(imgData.created_at);
+const getDate = (imgData) => {
+    const date = new Date(imgData.created_at); // Creates a new date object from the image date
+    const niceDate = date.toLocaleString("default", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    });
+    return niceDate;
+};
 
-  const markup = `
-    <figure class="image">
+// srcset="
+//   ${imgData.urls.full} ${imgData.width}w,
+//   ${imgData.urls.regular} 1080w,
+//   ${imgData.urls.small} 400w
+// "
+
+const buildImage = (imgData) => {
+    let srcset = `srcset="
+          ${imgData.urls.full} ${imgData.width}w
+          `;
+    if (imgData.urls.regular) {
+        srcset += `, ${imgData.urls.regular} 1080w`;
+    }
+    if (imgData.urls.small) {
+        srcset += `, ${imgData.urls.small} 400w`;
+    }
+    srcset += `"`;
+
+    const img = `
       <img
-        srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
-        "
-        sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-        src="${imgData.urls.regular}"
-        width="${imgData.width}"
-        height="${imgData.height}"
-        alt="${imgData.description}"
-        loading="lazy"
-      />
+      ${srcset}
+      sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
+      src="${imgData.urls.regular}"
+      width="${imgData.width}"
+      height="${imgData.height}"
+      alt="${imgData.description}"
+      loading="lazy"
+    />
+    `;
+    return img;
+};
+
+const Card = (data) => {
+    const imgData = data[0];
+
+    const markup = `
+    <figure class="image">
+      ${buildImage(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
@@ -37,11 +66,7 @@ const Card = (data) => {
           <p>
             Uploaded on
             <time class="image__date" datetime="${imgData.created_at}">
-            ${date.toLocaleString("default", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
+            ${getDate(imgData)}
             </time>.
           </p>
           <p>
@@ -54,7 +79,7 @@ const Card = (data) => {
     </figure>
   `;
 
-  mainContent.innerHTML = markup;
+    mainContent.innerHTML = markup;
 };
 
 Card(data);
